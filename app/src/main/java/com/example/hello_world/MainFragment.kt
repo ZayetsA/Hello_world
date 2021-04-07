a0a8325e9cd7b19f23be290c17bdef3913889e1f
Artem <zayats020401@gmail.com>
2021-04-07 10:29:11 +0300

    Initial commit

---
+++ app/src/main/java/com/example/hello_world/MainFragment.kt
@@ -0,0 +1,45 @@
+package com.example.hello_world
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import androidx.navigation.NavController
+import androidx.navigation.Navigation
+import com.example.hello_world.databinding.FragmentMainBinding
+
+class MainFragment : Fragment(), View.OnClickListener {
+    private var navigationController: NavController? = null
+    private var _binding: FragmentMainBinding? = null
+    private val binding get() = _binding!!
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        _binding = FragmentMainBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        navigationController = Navigation.findNavController(view)
+        binding.openRadio.setOnClickListener(this)
+        binding.openInputs.setOnClickListener(this)
+        binding.openButtons.setOnClickListener(this)
+    }
+
+    override fun onClick(v: View?) {
+        when (v!!.id) {
+            binding.openButtons.id -> navigationController?.navigate(R.id.action_mainFragment_to_buttonsFragment)
+            binding.openRadio.id -> navigationController?.navigate(R.id.action_mainFragment_to_checkFragment)
+            binding.openInputs.id -> navigationController?.navigate(R.id.action_mainFragment_to_inputsFragment)
+        }
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+}
